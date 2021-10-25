65907c889c7ea7e20b987f45ec7ff2606ea88b23
lofty5276uk <edward.lucas5276@hotmail.co.uk>
2021-10-25 19:23:04 +0000

    beautify js commit

--- assets/js/Project.js
+++ assets/js/Project.js
@@ -1,29 +1,29 @@
 function createRandom(length) {
     var consonants =
-    'bcdfghjklmnpqrstvwxyz'
-vowels = 'aeiou',
-    rand = function (limit) {
-        return
-        Math.floor(Math.random() *
-            limit);
-    },
-    i, word = '',
-    length = parseInt(length, 10),
-    consonants = consonants.split(''),
-    vowels = vowels.split('');
-for (i = 0; i < length / 2; i++) {
-    var randConsonant = consonants[rand(consonants.length)],
+        'bcdfghjklmnpqrstvwxyz'
+    vowels = 'aeiou',
+        rand = function (limit) {
+            return
+            Math.floor(Math.random() *
+                limit);
+        },
+        i, word = '',
+        length = parseInt(length, 10),
+        consonants = consonants.split(''),
+        vowels = vowels.split('');
+    for (i = 0; i < length / 2; i++) {
+        var randConsonant = consonants[rand(consonants.length)],
 
-        randVowels = vowels[rand(vowels.length)];
+            randVowels = vowels[rand(vowels.length)];
         word += === 0 ? randConsonants.toUpperCase() :
-        randConsonants;
+            randConsonants;
         word += i * 2 < length - 1 ? randVowels : "";
     }
     return word;
 }
-    $("#new").click(function()   {
-        $("#word").text("");
-        for(var p = 0; p < 1; p++){
-             $("word").append(createRandom($("#num").val()) + "<br/>");
-        }
-    })
+$("#new").click(function () {
+    $("#word").text("");
+    for (var p = 0; p < 1; p++) {
+        $("word").append(createRandom($("#num").val()) + "<br/>");
+    }
+})
