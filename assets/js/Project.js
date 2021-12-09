9764c4dc4d6f12f5fde0fcfc46ef09c1bde15c35
lofty5276uk <edward.lucas5276@hotmail.co.uk>
2021-12-09 20:22:29 +0000

    js script change commit

--- assets/js/Project.js
+++ assets/js/Project.js
@@ -51,7 +51,9 @@ var timeleft = 10;
 var downloadTimer = setInterval(function(){
   if(timeleft <= 0){
     clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
   }
-  document.getElementById("progressBar").value = 10 - timeleft;
   timeleft -= 1;
 }, 1000);
